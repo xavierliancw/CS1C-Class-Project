a2fe971eaef83526222388a746770b44b345b97b
Xavier Lian <xavierliancw@gmail.com>
2019-05-06 19:34:00 -0700

    Task #81 Progress, State Handling Pt 1
    - Updated VM for the vertex editor

--- src/viewmodels/vmeditorvertices.h
+++ src/viewmodels/vmeditorvertices.h
@@ -1,34 +1,57 @@
 #ifndef VMEDITORVERTICES_H
 #define VMEDITORVERTICES_H
 
+#include <QDebug>
 #include <QPoint>
 #include <QString>
 #include <QVector>
 #include <functional>
+#include <tuple>
+#include "models/ishape.h"
+#include "models/shapepolygon.h"
 
 class VMEditorVertices
 {
 public:
-    VMEditorVertices();
+    VMEditorVertices(std::function<void(bool)>vmDidHideAddVertexBt,
+                     std::function<void(bool)>vmDidEnableSubmitBt,
+                     std::function<void()>vmDidRefreshUI,
+                     std::function<void(IShape*)>vmDidGenerateNewShape,
+                     std::function<void(bool)>vmDidDisableVertexDeleting);
 
-    enum Mode {addPolygon, editPolygon,
-               addTriangle, editTriangle,
-               addPolyline, editPolyline,
-               addLine, editLine};
+    void resetStateUsing(IShape* shapeToBeEdited);
+    void resetStateUsing(IShape::ShapeType shapeTypeToGenerate);
 
     QString getLblTxtTitle() const;
-    QString getLblTxtVertexAmtPrompt() const;
     QString getBtTxtCancel() const;
     QString getBtTxtSubmit() const;
 
+    void finishEditing();
     void reorderVertex(int fromIndex, int toDestIndex);
+    void createVertex();
+    std::tuple<QString, QString> readVertextAt(int index) const;
+    void updateVertexAt(int index, QString newX, QString newY);
+    void removeVertexAt(int index);
 
 private:
-    std::function<void(int)>vmDidSetVertexSpinBoxValue;
-    std::function<void(bool)>vmDidHideVertexSpinBox;
-    std::function<void(bool)>vmDidEnableSubmitBt;
+    enum Mode {addPolygon, editPolygon,
+               addTriangle, editTriangle,
+               addPolyline, editPolyline,
+               addLine, editLine,
+               noMode};
 
-    QVector<QPoint> vertices;
+    std::function<void(bool)>vmDidHideAddVertexBt;
+    std::function<void(bool)>vmDidEnableSubmitBt;
+    std::function<void()>vmDidRefreshUI;
+    std::function<void(IShape*)>vmDidGenerateNewShape;
+    std::function<void(bool)>vmDidDisableVertexDeleting;
+
+    QVector<std::tuple<QString, QString>> vertices;
+    IShape* shapeBeingEdited = nullptr;
+    Mode currentEditMode = noMode;
+
+    IShape* generateNewShape();
+    void applyState();
 };
 
 #endif // VMEDITORVERTICES_H
